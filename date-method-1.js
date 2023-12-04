43a66337d3680b78cd60b21588101b0996468088
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-12-04 10:22:30 +0530

    date-method-1

---
+++ date-method-1.js
@@ -0,0 +1,17 @@
+document.write("calculate age"+"<br>")
+document.write("-------------"+"<br>")
+
+var a=new Date();
+var x=a.getFullYear();
+var b=new Date(2002,6,20);
+var y=b.getFullYear();
+
+document.write(x-y)
+
+
+
+
+
+
+
+
